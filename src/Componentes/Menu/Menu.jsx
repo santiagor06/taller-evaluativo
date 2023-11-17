7b3bf70553d3f6b8a2e226506249d6f66c77172a
john <johnmts22a@gmail.com>
2023-11-17 11:30:08 -0500

    router

--- src/Componentes/Menu/Menu.jsx
+++ src/Componentes/Menu/Menu.jsx
@@ -3,12 +3,30 @@ import "./Menu.css"
 const Menu = () => {
     return (<>
     <section className="menu">
-        <button className="botones">Login</button><br />
-        <button className="botones">Registro</button><br />
-        <button className="botones">Productos</button><br />
-        <button className="botones" >Clientes</button><br />
+        <Link to="login">
+        
+        <button to="login" className="botones">Login</button><br />
+        </Link>
+        <Link to="registro">
+        
+        <button  className="botones">Registro</button><br />
+        </Link>
+        <Link to="productos">
+        
+        <button  className="botones">Productos</button><br />
+        </Link>
+        <Link to="clientes">
+        
+        <button  className="botones" >Clientes</button><br />
+        </Link>
+        <Link >
+        
         <button className="botones">Cerrar sesion</button><br />
+        </Link>
+        <Link to="/">
+        
         <button className="botones">Home</button><br />
+        </Link>
     </section>
     
     </>  );
