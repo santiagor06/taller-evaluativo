3b64f47eff6c89724435b2800ceedf306a15d7e8
407 <407@academicos.cesde.edu.co>
2023-11-17 11:20:36 -0500

    Creacion cardProducto

--- src/Componentes/Productos/Productos.jsx
+++ src/Componentes/Productos/Productos.jsx
@@ -4,6 +4,21 @@ const Productos = () => {
     return ( <>
     <h1>Productos</h1>
     <ProductCard/>
+    <ProductCard/>
+    <ProductCard/>
+    <ProductCard/>
+    <ProductCard/>
+    <ProductCard/>
+    <ProductCard/>
+    <ProductCard/>
+    <ProductCard/>
+    <ProductCard/>
+    <ProductCard/>
+    <ProductCard/>
+    <ProductCard/>
+    <ProductCard/>
+    <ProductCard/>
+    <ProductCard/>
     </> );
 }
  
