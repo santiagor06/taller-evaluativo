d6af1edabba8d7b666a6cadcc67fb99d7cf39393
leruizr <leruizres1986@gmail.com>
2023-11-17 10:18:07 -0500

    se crea la rama clientes y se inicia a trabajar en el codigo de la misma

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import Menu from './Componentes/Menu/Menu'
+import Clientes from './Componentes/Clientes/Clientes'
 
 function App() {
 
   return (
     <>
     <Menu/>
+    <Clientes />
 
     </>
   )
---
+++ src/Componentes/Clientes/Clientes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Clientes/Clientes.css'
+
+const Clientes = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Clientes
