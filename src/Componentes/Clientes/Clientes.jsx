c9e395f0a8c5ef464debe6efc4fa6c4bba521793
leruizr <leruizres1986@gmail.com>
2023-11-17 10:57:47 -0500

    se crean las cards de los clientes

--- src/Componentes/Clientes/Clientes.jsx
+++ src/Componentes/Clientes/Clientes.jsx
@@ -1,12 +1,53 @@
 import React from 'react'
 import '../Clientes/Clientes.css'
+import cliente1 from '../../assets/cliente1.jpeg'
+import cliente2 from '../../assets/cliente2.jpeg'
+import cliente3 from '../../assets/cliente3.jpeg'
+import cliente4 from '../../assets/cliente4.jpeg'
+
 
 const Clientes = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <body>
+            <div class="parent">
+                <div class="card">
+                    <h1>Diana Gomez</h1>
+                    <p>Correo: diana@gamil.com</p>
+                    <p>Telefono: 555-55-55</p>
+                    <p>Ciudad: Medellín</p>
+                    <img src={cliente1} alt="" />
+                    <button>Agregar cliente</button>
+
+                </div>
+                <div class="card">
+                    <h1>Carlos Ramirez</h1>
+                    <p>Correo: diana@gamil.com</p>
+                    <p>Telefono: 555-55-55</p>
+                    <p>Ciudad: Medellín</p>
+                    <img src={cliente2} alt="" />
+                    <button>Agregar cliente</button>
+                </div>
+                <div class="card">
+                    <h1>DIANA ASPRILLA</h1>
+                    <p>Correo: diana@gamil.com</p>
+                    <p>Telefono: 32087643</p>
+                    <p>Ciudad: Medellín</p>
+                    <img src={cliente3} alt="" />
+                    <button>Agregar cliente</button>
+
+                </div>
+                <div class="card">
+                    <h1>PEDRO MORENO</h1>
+                    <p>Correo: pedrog@gamil.com</p>
+                    <p>Telefono: 645-80-55</p>
+                    <p>Ciudad: Medellín</p>
+                    <img src={cliente4} alt="" />
+                    <button>Agregar cliente</button>
+
+                </div>
+            </div>
+        </body>
+    )
 }
 
 export default Clientes
