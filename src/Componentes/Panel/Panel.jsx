8fa95ec73f928e384661ebe2638631ac8fb55347
Santiagotamayoz <130772765+Santiagotamayoz@users.noreply.github.com>
2023-11-17 11:25:42 -0500

    Panel final
    Panel-finalizado

--- src/Componentes/Panel/Panel.jsx
+++ src/Componentes/Panel/Panel.jsx
@@ -3,8 +3,12 @@ import './Panel.css'
 const Panel = () => {
     return (  <>
     <section className='section-panel'>
+        <div className='banner'>
         <h1 className='slogan'>DA UN PASO CON ESTILO </h1>
-        <img src="https://cms-cdn.thesolesupplier.co.uk/2022/08/adidas-x-bad-bunny-forum-low-blue-tint.jpg" alt="" />
+        </div>
+        <section className='bola'></section>
+        <img className='foto-slogan' src="https://cms-cdn.thesolesupplier.co.uk/2022/08/adidas-x-bad-bunny-forum-low-blue-tint.jpg" alt="" />
+        <img className='bad' src="https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/esAR/Images/00-originals-ss22-bbresponse-cp-logo-600-d_tcm216-861963.png" alt="" />
     </section>
     </>);
 }
