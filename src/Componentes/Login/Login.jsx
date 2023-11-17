3de66216d3618b355b86b3a1d526197025e5f154
Karen Malo <144466987+KarenMaloRamirez@users.noreply.github.com>
2023-11-17 11:13:29 -0500

    se completa el login con css

--- src/Componentes/Login/Login.jsx
+++ src/Componentes/Login/Login.jsx
@@ -1,19 +1,39 @@
 import React from 'react'
-
+import './Login.css'
 const Login = () => {
-  return (
-    <section>
-        <img src="" alt="Logo" />
-        <form action="">
-            <input  type="text"  placeholder="Usuario"></input>
-            <input  type="text"  placeholder="Correo"></input>
-            <input  type="text"  placeholder="Contraseña"></input>
-            <button type="submit">Ingresar</button>
-            <button type="submit">Registrar</button>
-        </form>
-    
-   </section>
-  )
+    return (
+        <section className='container'>
+
+            <form action="" >
+                <div className='login'>
+                    <img src="public\ImgLogin\ceshops.png" alt="Logo" />
+                    <div className='labels'>
+                        <label htmlFor="Usuario">Usuario</label>
+                        <input type="text" id='Usuario' ></input>
+                    </div>
+                    <div className='labels'>
+                        <label htmlFor="Correo">Correo</label>
+                        <input type="text" id='Correo'></input>
+                    </div>
+                    <div className='labels'>
+                        <label htmlFor="">Contraseña</label>
+                        <input type="text"></input>
+                    </div>
+                    <div className='buttoms'>
+                        <button type="submit">Registro</button>
+                    </div>
+                    <div>
+                        <p>¿Ya tienes una cuenta? <a href="">Inicia sesion</a></p>
+                    </div>
+                </div>
+            </form>
+            <section className='imagen'>
+                <img src="public\ImgLogin\imagenCompra.jpg" alt="" />
+            </section>
+
+        </section>
+
+    )
 }
 
 export default Login
