9ddfd63ee8197b995c706b67a8ae80536e0951a0
JhonMontoya93 <jhonmontoya821@gmail.com>
2023-11-17 11:08:16 -0500

    menu

--- src/Componentes/Menu/Menu.jsx
+++ src/Componentes/Menu/Menu.jsx
@@ -1,6 +1,15 @@
+import "./Menu.css"
 const Menu = () => {
     return (<>
-    Menu
+    <section className="menu">
+        <button className="botones">Login</button>
+        <button className="botones">Registro</button>
+        <button className="botones">Productos</button>
+        <button className="botones" >Clientes</button>
+        <button className="botones">Cerrar sesion</button>
+        <button className="botones">Home</button>
+    </section>
+    
     </>  );
 }
  
